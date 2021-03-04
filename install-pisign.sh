a7f19842f5a48a238927856a6627cda13863b7e4
Gavin Pease <gavinpease@gmail.com>
2021-03-04 13:54:49 -0700

    Create install-pisign.sh

---
+++ install-pisign.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [[ "$EUID" -ne 0 ]]; then
+echo "Run this script with sudo. "
+exit 1
+fi
+apt update -y
+apt install docker docker-compose git mongodb ffmpeg imagemagick -y
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+apt install nodejs -y
+git clone https://github.com/colloqi/pisignage-server
+mkdir -p media/_thumbnails
+chown pi:pi -R media
+cd pisignage-server
+npm install
+
+cat > /etc/systemd/system/pisign.service << EOF
+[Unit]
+Description=PiSignage server
+
+[Service]
+Type=simple
+WorkingDirectory=/home/pi/pisignage-server
+ExecStart=node server.js
+Restart=always
+User=pi
+
+[Install]
+WantedBy=network.target
+EOF
+chown pi:pi /home/pi/pisignage-server/data/releases/server-package.json
+chmod 755 /home/pi/pisignage-server/data/releases/server-package.json
+systemctl enable --now pisign
+exit
